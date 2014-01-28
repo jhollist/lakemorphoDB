8bf7314a0055c8bd75e9fad85c287d35b121dbd8
jhollist <jeff.w.hollister@gmail.com>
2014-01-28 12:04:17 -0500

    draft zip Lake Morpho function

--- zipLakeMorpho.R
+++ zipLakeMorpho.R
@@ -9,18 +9,18 @@
 #'  is outputLakeMorpho.zip
 #'
 #'  @examples
-#'  x<-c("ArkRedWhite11","California18","UpperMissouri10U")  
-getLakeMorpho<-function(hucVec,outFile="outputLakeMorpho.zip"){
+#'  x<-c("ArkRedWhite11","California18") 
+#'  getLakeMorpho(x) 
+#'  #Example for Eastern TNC Lakes
+#'  eTNCRegions <- c()
+zipLakeMorpho<-function(hucVec,outFile="outputLakeMorpho.zip"){
   # Assumes location is AED Network drive
   basePath<-"L:\\Priv\\LakeMorphometry"
-  dirs<-list.files()[list.files()%in%hucVec]
+  dirs<-list.files(basePath)[list.files(basePath)%in%hucVec]
   myFiles<-NULL
   for(dir in dirs){
     myPath<-paste(basePath,dir,"lakemorphodata\\FinalLakeMorpho",sep="\\")
-    myFiles<-c(myFiles,list.files(path=myPath,pattern=dir))
+    myFiles<-c(myFiles,list.files(path=myPath,pattern=dir,full.names=T))
   }
-  myFiles
-  
-  
-  
+  zip(outFile,myFiles,flags="-j")
 }  
